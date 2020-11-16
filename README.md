5787a6628210d1cb4350f5301960950cc613b077
Natnael Eshetu <dragoneth.ion@gmail.com>
2020-11-16 05:27:12 +0300

    Added platform.hpp and related tests

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A well organized C++ OpenGL wrangler library
 
 #### Library Headers
 - [axl.glw/lib.hpp](/include/axl.glw/lib.hpp) - The library header. Contains library version.
+- [axl.glw/platform.hpp](/include/axl.glw/platform.hpp) - Declares platform and ABI.
---
+++ tests/platform.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <axl.glw/platform.hpp>
+
+int main()
+{
+	using namespace axl::glw;
+	const char *CSTR_PLATFORM = 0, *CSTR_ABI = 0;
+	switch (PLATFORM)
+	{
+		case PLATFORM_WINDOWS: CSTR_PLATFORM = "Windows"; break;
+		case PLATFORM_IPHONE: CSTR_PLATFORM = "Iphone"; break;
+		case PLATFORM_ANDROID: CSTR_PLATFORM = "Android"; break;
+		case PLATFORM_MAC: CSTR_PLATFORM = "Mac"; break;
+		case PLATFORM_LINUX: CSTR_PLATFORM = "Linux"; break;
+		case PLATFORM_UNIX: CSTR_PLATFORM = "Unix"; break;
+		case PLATFORM_POSIX: CSTR_PLATFORM = "Posix"; break;
+		case PLATFORM_UNKNOWN:
+		default: CSTR_PLATFORM = "Unknown";
+	}
+	switch (ABI)
+	{
+		case ABI_WIN32: CSTR_ABI = "Win32"; break;
+		case ABI_WIN64: CSTR_ABI = "Win64"; break;
+		case ABI_ANDROID_ARM: CSTR_ABI = "Android_arm"; break;
+		case ABI_ANDROID_ARM64: CSTR_ABI = "Android_arm64"; break;
+		case ABI_ANDROID_X86: CSTR_ABI = "Android_x86"; break;
+		case ABI_ANDROID_X86_64: CSTR_ABI = "Android_x86_64"; break;
+		case ABI_ANDROID_MIPS64: CSTR_ABI = "Android_mips64"; break;
+		case ABI_ANDROID_MIPS: CSTR_ABI = "Android_mips"; break;
+		case ABI_UNKNOWN:
+		default: CSTR_ABI = "Unknown";
+	}
+	printf("Platform: %s\n", CSTR_PLATFORM);
+	printf("ABI: %s\n", CSTR_ABI);
+	return 0;
+}
